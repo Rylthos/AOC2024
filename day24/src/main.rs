c0ccb10ebadea9dd66f6e155d1ae4caa0a2262f3
Aaron Danton <aarondanton04@hotmail.com>
2024-12-27 20:37:58 +0000

    Day 24 - Part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
@@ -68,14 +69,12 @@ fn parse_input(
     (inputs, targets)
 }
 
-fn first_char(str: &String) -> char {
-    str.chars().nth(0).unwrap()
-}
-
 fn calc_value(
     target: &String,
     registers: &mut HashMap<String, bool>,
     targets: &HashMap<String, (String, Operation, String)>,
+    added: &mut HashSet<(String, String)>,
+    lookup: &mut HashMap<String, String>,
 ) -> bool {
     if let Some(v) = registers.get(target) {
         return *v;
@@ -83,20 +82,59 @@ fn calc_value(
 
     let (reg1, op, reg2) = targets.get(&target.clone()).unwrap();
 
-    // let str = match op {
-    //     Operation::AND => "AND",
-    //     Operation::OR => "OR",
-    //     Operation::XOR => "XOR",
-    // };
+    let (reg1, reg2) = if reg1 < reg2 {
+        (reg1, reg2)
+    } else {
+        (reg2, reg1)
+    };
 
-    let v1 = calc_value(reg1, registers, targets);
-    let v2 = calc_value(reg2, registers, targets);
+    let v1 = calc_value(reg1, registers, targets, added, lookup);
+    let v2 = calc_value(reg2, registers, targets, added, lookup);
+
+    let l = lookup.clone();
+
+    let r1 = l.get(reg1).unwrap_or(reg1);
+    let r2 = l.get(reg2).unwrap_or(reg2);
+
+    // let (r1, r2) = if r1 < r2 { (r1, r2) } else { (r2, r1) };
+
+    let mut new_target = String::new();
+
+    let str = match op {
+        Operation::AND => "AND",
+        Operation::OR => "OR",
+        Operation::XOR => "XOR",
+    };
+
+    if !l.contains_key(target) {
+        if r1[1..] == r2[1..] {
+            if r1[..1] == *"x" && r2[..1] == *"y" {
+                let rv: u64 = r1[1..].parse().unwrap();
+
+                new_target = match op {
+                    Operation::AND => format!("A{:02}", rv),
+                    Operation::OR => format!("O{:02}", rv),
+                    Operation::XOR => format!("X{:02}", rv),
+                };
+
+                lookup.insert(target.to_string(), new_target.clone());
+            }
+        } else {
+            new_target = lookup.get(target).unwrap_or(target).to_string();
+        }
+    } else {
+        new_target = lookup.get(target).unwrap().to_string();
+    }
+
+    println!("{r1} {str} {r2} = {new_target} [{target}]");
 
     let final_value = match op {
         Operation::AND => v1 & v2,
         Operation::OR => v1 | v2,
         Operation::XOR => v1 ^ v2,
     };
+
+    registers.insert(target.to_string(), final_value);
 
     return final_value;
 }
@@ -107,6 +145,8 @@ fn calc_output(
 ) -> u64 {
     let mut inputs = inputs.clone();
 
+    let mut lookup: HashMap<String, String> = HashMap::new();
+
     let mut outputs: BTreeSet<String> = BTreeSet::new();
     for (target, _) in targets {
         if target.chars().nth(0).unwrap() == 'z' {
@@ -114,12 +154,13 @@ fn calc_output(
         }
     }
 
-    let mut output = 0;
-    for o in outputs.iter() {
-        output = output << 1;
+    let mut added: HashSet<(String, String)> = HashSet::new();
+    let mut output: u64 = 0;
+    for (i, o) in (0..).zip(outputs.iter()) {
+        let v = calc_value(o, &mut inputs, &targets, &mut added, &mut lookup);
+        output |= ((v as u64) << i) as u64;
 
-        let v = calc_value(o, &mut inputs, &targets);
-        output |= v as u64;
+        println!();
     }
 
     output
@@ -137,6 +178,7 @@ fn main() {
     // let (inputs, targets) = parse_input("Example.txt");
     // let (inputs, targets) = parse_input("SimpleExample.txt");
     let (inputs, targets) = parse_input("Input.txt");
+    // let (inputs, targets) = parse_input("Changed.txt");
 
     let p1 = part1(&inputs, &targets);
     println!("Part 1: {p1}");
